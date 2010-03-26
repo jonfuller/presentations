0d0d681cd5cd3490cd68e52bfda73a96da62d11d
Jon Fuller <fullerjc@gmail.com>
2010-03-26 10:53:30 -0400

    refactor a little more

--- dependencies/source.rb
+++ dependencies/source.rb
@@ -9,30 +9,51 @@ class Highlight
     @separator = Config::CONFIG['host_os'] == "mswin32" ? '\\\\' : '/'
   end
 
-  def highlight_to_svg(src_file, svg_file, opts)
-    options = opts.map { |h| "--#{h[0].to_s.tr('_', '-')}=\"#{h[1]}\"" }.join(" ")
-    system("highlight < \"#{src_file.tr('/', @separator)}\" > \"#{svg_file.tr('/', @separator)}\" --svg #{options}")
+  def highlight_to_rtf(src_file, rtf_file, opts)
+    highlight("rtf", src_file, out_file, opts)
+  end
 
-    # merge stylesheet into the SVG
+  def highlight_to_svg(src_file, svg_file, opts)
+    highlight("svg", src_file, svg_file, opts)
+
+    merge_stylesheet(svg_file, "highlight.css")
+    adjust_linespacing(src_file, svg_file, opts[:font_size], opts[:height], opts[:width])
+  end
+
+  def highlight(format, src_file, out_file, opts)
+    options = opts.map { |h| "--#{h[0].to_s.tr('_', '-')}=\"#{h[1]}\"" }.join(" ")
+    system("highlight < \"#{src_file.tr('/', @separator)}\" > \"#{out_file.tr('/', @separator)}\" --#{format} #{options}")
+  end
+
+  def merge_stylesheet(svg_file, css_filename)
     svg = File.open(svg_file).readlines()
-    css = File.open("highlight.css").readlines()
+    css = File.open(css_filename).readlines()
 
     svg.insert(4, "<style>")
     svg.insert(5, "</style>")
     svg.insert(5, css)
     svg.flatten!
-    
-    code_height = (File.open(src_file) { |src| src.readlines() }).length*opts[:font_size]+20
-    code_width = (File.open(src_file) { |src| src.readlines() }).map { |line| line.length}.max*(opts[:font_size]*2/3)
+
+    File.open(svg_file, "w") do |f|
+      svg.each {|line| f.write(line)}
+      f.close
+    end
+  end
+
+  def adjust_linespacing(src_file, svg_file, font_size, height, width)
+    svg = File.open(svg_file).readlines()
+
+    code_height = (File.open(src_file) { |src| src.readlines() }).length*font_size+20
+    code_width = (File.open(src_file) { |src| src.readlines() }).map { |line| line.length}.max*(font_size*2/3)
 
     # make the line spacing a little closer
-    y_start = [10, ((opts[:height ]- code_height)/2)].max
-    x_start = [10, ((opts[:width ]- code_width)/2)].max
+    y_start = [10, ((height- code_height)/2)].max
+    x_start = [10, ((width- code_width)/2)].max
     index = -1
     svg.each do |line| 
       if /.*text.*(y=\")([0-9]+)(\")/.match(line)
         index = index+1
-        line.gsub!(/(.*text.*y=\")([0-9]+)(\")/, '\1' + (y_start + (index*opts[:font_size])).to_s + '\3')
+        line.gsub!(/(.*text.*y=\")([0-9]+)(\")/, '\1' + (y_start + (index*font_size)).to_s + '\3')
         line.gsub!(/(.*text.*x=\")([0-9]+)(\")/, '\1' + x_start.to_s + '\3')
       end
     end
